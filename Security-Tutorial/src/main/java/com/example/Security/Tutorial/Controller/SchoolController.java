e0ccc8001e0978c91383ff3c228137dd287aa393
SonithKumarR <156375453+SonithKumarR@users.noreply.github.com>
2024-10-22 20:36:23 +0530

    Add files via upload
    Added new Project

---
+++ Security-Tutorial/src/main/java/com/example/Security/Tutorial/Controller/SchoolController.java
@@ -0,0 +1,26 @@
+package com.example.Security.Tutorial.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class SchoolController {
+
+    @GetMapping("/home")
+    public String home(){
+        return "Welcome HOME";
+    }
+
+    @GetMapping("/student")
+    public String student(){
+        return "Welcome STUDENT";
+    }
+    @GetMapping("/admin")
+    public String admin(){
+        return "Welcome ADMIN";
+    }
+}
+
